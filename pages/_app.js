9d11992113dcf73c6c44edd385f78aa99462f2e5
nyac-1 <routusampath@yahoo.com>
2021-11-15 21:35:46 +0530

    nextJS sandbox

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss';
+import React, {useState, useEffect} from 'react';
+import {Layout} from "../components/index.js";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <Layout>
+      <Component {...pageProps}/>
+    </Layout>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import {PostCard, PostWidget, Categories, Layout, Header} from '../components/index.js';
 
-const posts = [
-  {title: "ugh", excerpt: "idek"},
-]
+const posts =[
+  {title: "React Testing", excerpt: "Learn React Testing"},
+  {title: "React with Tailwind", excerpt: "Learn React testing with Tailwind"}
+];
 
 export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <div className = "grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index)=>{
-          {post.title}
-          {post.excerpt}
-        })}
-      </div> */}
+      <div className = "grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className = "lg:col-span-8 col-span-1">
+          {posts.map((post, index)=> <PostCard post = {post} key = {post.title}/>)}
+        </div>
+
+        <div className = "lg:col-span-4 col-span-1">
+          <div className = "lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
